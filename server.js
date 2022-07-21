c52a612a0268a07cdcf9b42aef72a42217eba89d
Diego Hernandez <diegofootball19@gmail.com>
2022-07-21 12:08:39 -0500

    renamed func

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 mongoose.connect(process.env.DATABASE_URL, {}, () => {
   console.log("connected to db");
 });
+
 app.get("/", (req, res) => {
   res.render("index");
 });
@@ -26,9 +27,10 @@ app.post("/upload", upload.single("file"), async (req, res) => {
   const file = await File.create(fileData);
   res.render("index", { fileLink: `${req.headers.origin}/file/${file.id}` });
 });
-app.route("/file/:id").get(handelDonwload).post(handelDonwload);
 
-async function handelDonwload(req, res) {
+app.route("/file/:id").get(handelDownload).post(handelDownload);
+
+async function handelDownload(req, res) {
   const file = await File.findById(req.params.id);
   if (file.password != null) {
     if (req.body.password == null) {
@@ -46,4 +48,5 @@ async function handelDonwload(req, res) {
 
   res.download(file.path, file.originalName);
 }
+
 app.listen(process.env.PORT);
